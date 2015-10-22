4bb36625ffd041c996361663c3bad0fe2b5070f4
Justin Zimmerman <zimmy130@gmail.com>
2015-10-22 15:17:51 -0400

    passed player view test

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -1,11 +1,16 @@
 // PlayerView.js - Defines a backbone view class for the music player.
 var PlayerView = Backbone.View.extend({
 
+  //WHAT MODEL IS THIS VIEW CONNECTED TO?
+
   // HTML5 (native) audio tag is being used
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
   el: '<audio controls autoplay />',
 
   initialize: function() {
+    this.$el.on('ended', function() {
+      this.model.ended();
+    }.bind(this));
   },
 
   setSong: function(song){
@@ -18,3 +23,11 @@ var PlayerView = Backbone.View.extend({
   }
 
 });
+
+//Notes
+  //PlayerView dequeues a song when finished playing & plays the next song
+
+//Change
+  //in initialize:
+    //plugged in the on event - this.$el.on('ended', function() {...
+    //binded this to the correct context
